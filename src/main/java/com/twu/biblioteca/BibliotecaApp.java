8b11badb80b47f5d926c27b0739bec8d3b182b95
Pranali Satalgaonkar <pranali.s@thoughtworks.com>
2020-02-07 18:42:31 +0530

    Requirement-1.2 | Add | Showing a list of all library books after welcome message.

--- src/main/java/com/twu/biblioteca/BibliotecaApp.java
+++ src/main/java/com/twu/biblioteca/BibliotecaApp.java
@@ -1,10 +1,13 @@
 package com.twu.biblioteca;
 
 import java.io.PrintStream;
+import java.util.List;
 
 public class BibliotecaApp {
 
-    public BibliotecaApp(PrintStream printStream) {
-        printStream.print("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!");
+    public BibliotecaApp(PrintStream printStream, List<String> bookList) {
+        printStream.println("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!");
+        for(String book : bookList)
+            printStream.println(book);
     }
 }
--- src/test/java/com/twu/biblioteca/BibliotecaAppTest.java
+++ src/test/java/com/twu/biblioteca/BibliotecaAppTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -13,10 +16,21 @@ public class BibliotecaAppTest {
 
     @Test
     public void testShouldPrintWelcomeMessageOnConsole() {
-        String expectedOutput = "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!" ;
+        String expectedOutput = "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!\n" ;
 
-        BibliotecaApp bibliotecaApp = new BibliotecaApp(new PrintStream(outContent));
+        BibliotecaApp bibliotecaApp = new BibliotecaApp(new PrintStream(outContent), Collections.emptyList());
 
         assertEquals(expectedOutput, outContent.toString());
+    }
+
+    @Test
+    public void testShouldEnlistAllLibraryBooks() {
+        StringBuilder expectedOutput = new StringBuilder("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!\n");
+        List<String> bookList = Arrays.asList("Pride and Prejudice", "The Diary of Anne Frank", "To Kill a Mockingbird", "Little Women");
+        for(String book : bookList)
+            expectedOutput.append(book).append("\n");
+        BibliotecaApp bibliotecaApp = new BibliotecaApp(new PrintStream(outContent), bookList);
+
+        assertEquals(expectedOutput.toString(), outContent.toString());
     }
 }
