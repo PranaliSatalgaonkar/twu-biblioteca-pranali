2fd2ad2b3b6fcf88ff448e526dbba77df400b61f
Pranali Satalgaonkar <pranali.s@thoughtworks.com>
2020-02-07 18:47:46 +0530

    Requirement-1.2 | Refactor | Different methods introduced for showing welcome message and books.

--- src/main/java/com/twu/biblioteca/BibliotecaApp.java
+++ src/main/java/com/twu/biblioteca/BibliotecaApp.java
@@ -5,9 +5,23 @@ import java.util.List;
 
 public class BibliotecaApp {
 
+    PrintStream printStream;
+    List<String> bookList;
+
     public BibliotecaApp(PrintStream printStream, List<String> bookList) {
+        this.printStream = printStream;
+        this.bookList = bookList;
+
+        showWelcomeMessage();
+        showBookList();
+    }
+
+    void showWelcomeMessage() {
         printStream.println("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!");
-        for(String book : bookList)
+    }
+
+    void showBookList() {
+        for (String book : bookList)
             printStream.println(book);
     }
 }
--- src/test/java/com/twu/biblioteca/BibliotecaAppTest.java
+++ src/test/java/com/twu/biblioteca/BibliotecaAppTest.java
@@ -27,7 +27,7 @@ public class BibliotecaAppTest {
     public void testShouldEnlistAllLibraryBooks() {
         StringBuilder expectedOutput = new StringBuilder("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!\n");
         List<String> bookList = Arrays.asList("Pride and Prejudice", "The Diary of Anne Frank", "To Kill a Mockingbird", "Little Women");
-        for(String book : bookList)
+        for (String book : bookList)
             expectedOutput.append(book).append("\n");
         BibliotecaApp bibliotecaApp = new BibliotecaApp(new PrintStream(outContent), bookList);
 
