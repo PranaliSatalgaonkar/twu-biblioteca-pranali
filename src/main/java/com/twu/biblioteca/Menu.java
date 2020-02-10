5a47335d0f17afc1a932b40a5ae6739aadcc2bb2
Pranali Satalgaonkar <pranali.s@thoughtworks.com>
2020-02-10 08:02:14 +0530

    Refactor | Displaying choices before giving input.

--- src/main/java/com/twu/biblioteca/Menu.java
+++ src/main/java/com/twu/biblioteca/Menu.java
@@ -11,7 +11,7 @@ public class Menu {
     }
 
     public void displayMenu() {
-        System.out.print("\nMENU:\n\n1. Display books in library.\n2. Checkout Book.\n3. Return Book.\n4. Quit Application.\n\nEnter your choice: ");
+        System.out.println("\nMENU:\n\n1. Display books in library.\n2. Checkout Book.\n3. Return Book.\n4. Quit Application.\n\nEnter your choice : ");
         Scanner scanner = new Scanner(System.in);
         int option = scanner.nextInt();
 
@@ -35,18 +35,18 @@ public class Menu {
             library.displayBookList();
         }
         if (option == 2) {
-            System.out.print("\nEnter title of the book: ");
+            System.out.println("Enter title of the book: ");
             Scanner scanner = new Scanner(System.in);
             String bookTitle = scanner.nextLine();
             library.checkoutBook(bookTitle);
         }
         if (option == 3) {
             Scanner scanner = new Scanner(System.in);
-            System.out.print("\nEnter title of the book : ");
+            System.out.println("Enter title of the book : ");
             String bookTitle = scanner.nextLine();
-            System.out.print("\nEnter author : ");
-            String bookAuthor= scanner.nextLine();
-            System.out.print("\nEnter publication year : ");
+            System.out.println("Enter author : ");
+            String bookAuthor = scanner.nextLine();
+            System.out.println("Enter publication year : ");
             int bookPublicationYear = scanner.nextInt();
             library.returnBook(new Book(bookTitle, bookAuthor, bookPublicationYear));
         }
