1f38e57f0e1f529df5028becabda6f457307adee
Pranali Satalgaonkar <pranali.s@thoughtworks.com>
2020-02-10 21:35:24 +0530

    Refactor | Converting ArrayList of books in Inventory class to List type.

--- src/main/java/com/twu/biblioteca/Inventory.java
+++ src/main/java/com/twu/biblioteca/Inventory.java
@@ -1,17 +1,17 @@
 package com.twu.biblioteca;
 
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Inventory {
-    ArrayList<Book> inventoryBookList; //TODO; private, List instead of arraylist
+    private List<Book> inventoryBookList;
 
     public Inventory() {
-        inventoryBookList = new ArrayList<>(Arrays.asList(new Book("Pride and Prejudice", "Jane Austen", 1813),
+        inventoryBookList = Arrays.asList(new Book("Pride and Prejudice", "Jane Austen", 1813),
                 new Book("The Diary of a Young Girl", "Anne Frank", 1947),
                 new Book("To Kill a Mockingbird", "Harper Lee", 1960),
                 new Book("Little Women", "Louisa May Alcott", 1868),
-                new Book("The Alchemist", "Paulo Coelho", 1988))); //TODO no need for arraylist
+                new Book("The Alchemist", "Paulo Coelho", 1988));
     }
 
     public boolean isInInventory(Book book) {
