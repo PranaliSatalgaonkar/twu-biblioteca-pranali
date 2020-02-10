a233ce97b4e702d39f3dcec93ebbdf69dc5a3a14
Pranali Satalgaonkar <pranali.s@thoughtworks.com>
2020-02-10 21:20:57 +0530

    Refactor | Removing unnecessary imports from Book class.

--- src/test/java/com/twu/biblioteca/BookTest.java
+++ src/test/java/com/twu/biblioteca/BookTest.java
@@ -1,6 +1,5 @@
 package com.twu.biblioteca;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -10,7 +9,7 @@ class BookTest {
     @Test
     public void testShouldReturnBookDetails() {
         Book book = new Book("The Spy", "Paulo Coelho", 2016);
-        String expectedOutput = book.title +" "+book.author+" "+book.publicationYear;
+        String expectedOutput = book.title + " " + book.author + " " + book.publicationYear;
 
         assertEquals(expectedOutput, book.getBookDetails());
     }
