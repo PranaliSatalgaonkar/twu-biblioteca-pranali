8fcb86bfdc093a05d1a5a73c58907dbbbbd0267c
Pranali Satalgaonkar <pranali.s@thoughtworks.com>
2020-02-11 16:53:25 +0530

    Requirement-2.1 | Add | Getting movie details.

--- src/main/java/com/twu/biblioteca/Movie.java
+++ src/main/java/com/twu/biblioteca/Movie.java
@@ -1,15 +1,19 @@
 package com.twu.biblioteca;
 
 public class Movie {
-    private String name;
-    private int releaseYear;
-    private String director;
-    private String rating;
+    protected String name;
+    protected int releaseYear;
+    protected String director;
+    protected String rating;
 
     public Movie(String name, int releaseYear, String director, String rating) {
         this.name = name;
         this.releaseYear = releaseYear;
         this.director = director;
         this.rating = rating;
+    }
+
+    public String getDetails() {
+        return name + " " + releaseYear + " " + director + " " + rating;
     }
 }
--- src/test/java/com/twu/biblioteca/MovieTest.java
+++ src/test/java/com/twu/biblioteca/MovieTest.java
@@ -5,4 +5,12 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MovieTest {
+
+    @Test
+    public void testShouldReturnMovieDetails() {
+        Movie movie = new Movie("Parasite", 2019, "Bong Joon-ho", "10");
+        String expectedOutput = movie.name + " " + movie.releaseYear + " " + movie.director + " " + movie.rating;
+
+        assertEquals(expectedOutput, movie.getDetails());
+    }
 }
