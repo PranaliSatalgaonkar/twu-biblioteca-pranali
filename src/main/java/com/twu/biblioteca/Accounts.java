bb9eef349eb3034b90bfb842972c32c11779ce6a
Pranali Satalgaonkar <pranali.s@thoughtworks.com>
2020-02-12 09:18:42 +0530

    Requirement-2.3 | Add | Adding Accounts class.

---
+++ src/main/java/com/twu/biblioteca/Accounts.java
@@ -0,0 +1,17 @@
+package com.twu.biblioteca;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Accounts {
+    private List<User> userList;
+
+    Accounts() {
+        userList = new ArrayList<>(Arrays.asList(new User("123-4567", "abcd"),
+                new User("111-1111", "1111"),
+                new User("123-9842", "pass123"),
+                new User("158-0106", "password"),
+                new User("787-5008", "xyz123")));
+    }
+}
